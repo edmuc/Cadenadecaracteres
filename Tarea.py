3e09dc3daf1bb983003519f899a91a59b4bc9905
edmuc <jemurillo@unal.edu.co>
2025-09-15 07:13:36 +0000

    Actualizando

--- Tarea.py
+++ Tarea.py
@@ -1,3 +1,4 @@
+
 # Ejemplo 1:
 palabra = input("Ingrese una palabra: ")
 
@@ -7,11 +8,54 @@ print("La palabra", palabra, "tiene", len(palabra), "caracteres")
 print("El primer carácter es:", palabra[0])
 print("El último carácter es:", palabra[-1])
 
-# Ejemplo 2:
-frase = input("\nIngrese una frase: ")
+# Ejemplo 2: 
+var = input("\nIngrese una frase: ")
 
 print("\nEjemplo 2")
-print("El tipo de dato de 'frase' es:", type(frase))
-print("La frase completa es:", frase)
-print("La frase tiene", len(frase), "caracteres")
-print("Los primeros 5 caracteres son:", frase[0:5])
+print("El tipo de dato de 'frase' es:", type(var))
+print("La frase completa es:", var)
+print("La frase tiene", len(var), "caracteres")
+print("Los primeros 5 caracteres son:", var[0:5])
+
+
+# Ejemplo 3:
+
+print("\nDescubre la palabra oculta")
+while True:
+    var2 = input("Intente adivinar la palabra: ")
+    
+    if var2 == var:
+        print("¡Usted descubrió el mensaje oculto!")
+        break  # termina el bucle si acierta
+    else:
+        print("Palabra equivocada. Intente de nuevo.")
+cat > Tarea.py << 'EOF'
+# Ejemplo 1:
+palabra = input("Ingrese una palabra: ")
+
+print("\nEjemplo 1")
+print("El tipo de dato de 'palabra' es:", type(palabra))
+print("La palabra", palabra, "tiene", len(palabra), "caracteres")
+print("El primer carácter es:", palabra[0])
+print("El último carácter es:", palabra[-1])
+
+# Ejemplo 2: 
+var = input("\nIngrese una frase: ")
+
+print("\nEjemplo 2")
+print("El tipo de dato de 'frase' es:", type(var))
+print("La frase completa es:", var)
+print("La frase tiene", len(var), "caracteres")
+print("Los primeros 5 caracteres son:", var[0:5])
+
+# Ejemplo 3:
+
+print("\nDescubre la palabra oculta")
+while True:
+    var2 = input("Intente adivinar la palabra: ")
+    
+    if var2 == var:
+        print("¡Usted descubrió el mensaje oculto!")
+        break  # termina el bucle si acierta
+    else:
+        print("Palabra equivocada. Intente de nuevo.")
