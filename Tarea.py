66be69e344cdfa9fad36d21bcfbecfaf5504fd04
edmuc <jemurillo@unal.edu.co>
2025-09-15 06:43:54 +0000

    Actualizando Tarea.py con ejemplos de cadenas

--- Tarea.py
+++ Tarea.py
@@ -1,1 +1,17 @@
-print('Hola GitHub')
+# Ejemplo 1:
+palabra = input("Ingrese una palabra: ")
+
+print("\nEjemplo 1")
+print("El tipo de dato de 'palabra' es:", type(palabra))
+print("La palabra", palabra, "tiene", len(palabra), "caracteres")
+print("El primer carácter es:", palabra[0])
+print("El último carácter es:", palabra[-1])
+
+# Ejemplo 2:
+frase = input("\nIngrese una frase: ")
+
+print("\nEjemplo 2")
+print("El tipo de dato de 'frase' es:", type(frase))
+print("La frase completa es:", frase)
+print("La frase tiene", len(frase), "caracteres")
+print("Los primeros 5 caracteres son:", frase[0:5])
